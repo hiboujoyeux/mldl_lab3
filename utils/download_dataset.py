5e74b486ad9a9e436afb69aafcdb5603204bd05d
hiboujoyeux <158048428+hiboujoyeux@users.noreply.github.com>
2025-03-18 17:15:05 +0100

    Update download_dataset.py

--- utils/download_dataset.py
+++ utils/download_dataset.py
@@ -1,5 +1,4 @@
 import os
 
-os.system("cd ./dataset/")
-os.system("wget http://cs231n.stanford.edu/tiny-imagenet-200.zip")
-os.system("unzip tiny-imagenet-200.zip -d tiny-imagenet")
+os.system("cd ./dataset/ && wget http://cs231n.stanford.edu/tiny-imagenet-200.zip")
+os.system("cd ./dataset/ && unzip tiny-imagenet-200.zip -d tiny-imagenet")
